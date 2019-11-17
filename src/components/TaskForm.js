af573eae132277956f6240da92e8837cb2ec64ee
Giggs3244 <bryan.bedoy@gmail.com>
2019-11-17 12:07:38 -0500

    Se crea un formulario para la creacion de tareas, se definen los handle change y submit

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -2,10 +2,43 @@ import React, { Component } from 'react';
 
 export default class TaskForm extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            nombre: '',
+            descripcion: ''
+        }
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
     render() {
         return (
-            <form>
-                <input type="text" placeholder="Nombre de la tarea"/>
+            <form onSubmit={this.handleSubmit}>
+                <input 
+                    name="nombre"
+                    type="text" 
+                    placeholder="Nombre de la tarea"
+                    value={this.state.nombre}
+                    onChange={this.handleChange}
+                ></input>
+                <br></br>
+                <textarea
+                    name="descripcion"
+                    placeholder="Descripcion de la tarea"
+                    value={this.state.descripcion}
+                    onChange={this.handleChange}
+                ></textarea>
+                <br></br>
+                <input type="submit" value="Crear tarea"></input>
             </form>
         );
     }
